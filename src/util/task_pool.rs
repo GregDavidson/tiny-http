ef7c20ca224ea37f4a11252b37d0b56e13c86534
KeithShillington <Keith.Shillington@gmail.com>
2019-01-18 15:59:29 -0800

    Added clarifying comments; and new possible design

--- src/util/task_pool.rs
+++ src/util/task_pool.rs
@@ -21,7 +21,9 @@ use std::thread;
 /// Manages a collection of threads.
 ///
 /// A new thread is created every time all the existing threads are full.
-/// Any idle thread will automatically die after a few seconds.
+/// Excessive (more than MIN_THREADS) threads die after IDLE_TIME of no work.
+// Confusion: Should be ThreadPool or WorkerPool, as we use task to
+// refer to something for a Worker Thread to do!!
 pub struct TaskPool {
     sharing: Arc<Sharing>,
 }
@@ -34,14 +36,16 @@ struct Sharing {
     condvar: Condvar,
 
     // number of total worker threads running
-    active_tasks: AtomicUsize,
+    active_tasks: AtomicUsize,  // active_worker_count??
 
     // number of idle worker threads
-    waiting_tasks: AtomicUsize,
+    waiting_tasks: AtomicUsize, // waiting_worker_count??
 }
 
 /// Minimum number of active threads.
+// How can we move this to the .toml file??
 static MIN_THREADS: usize = 4;
+static IDLE_TIME: Duration = Duration::from_millis(5000);
 
 struct Registration<'a> {
     nb: &'a AtomicUsize
@@ -96,42 +100,54 @@ impl TaskPool {
         let sharing = self.sharing.clone();
 
         thread::spawn(move || {
-            let sharing = sharing;
+            let sharing = sharing; // redundant? move will do this!
+            // increment active_worker_count while we're alive
             let _active_guard = Registration::new(&sharing.active_tasks);
 
-            if initial_fn.is_some() {
-                let mut f = initial_fn.unwrap();
+            if initial_fn.is_some() { // could be an if let
+                let mut f = initial_fn.unwrap(); 
                 f();
             }
+            // if let mut Some(f) = initial_fn { f(); }
 
-            loop {
+          loop {
+            // receive tasks, and run them
+            // if task queue is empty, wait or, if we've got plenty of threads, terminate
                 let mut task: Box<FnMut() + Send> = {
+                    // this todo is a MutexGuard on the task queue
                     let mut todo = sharing.todo.lock().unwrap();
 
-                    let task;
-                    loop {
+                    let task; // the task we hope to obtain
+                    loop { // until we get a task
                         if let Some(poped_task) = todo.pop_front() {
-                            task = poped_task;
+                            task = poped_task; // we got one!
                             break;
                         }
+                        // we are a new idle worker, so increment waiting_worker_count
+                        // while we're in this loop
                         let _waiting_guard = Registration::new(&sharing.waiting_tasks);
 
+                        // received only means we received a notify (on the condition variable)
+                        // we should loop around and see if we can be the lucky winner of a task
+                        // todo is the mutex guard returned from the wait on the condition variable
                         let received = if sharing.active_tasks.load(Ordering::Acquire)
                                                 <= MIN_THREADS
-                        {
+                        {   // we will wait as long as necessary
+                            // do we really need to reassign our MutexGuard??
                             todo = sharing.condvar.wait(todo).unwrap();
                             true
 
-                        } else {
+                        } else { // we might be redundant, wait only briefly
                             let (new_lock, waitres) = sharing.condvar
-                                                             .wait_timeout(todo, Duration::from_millis(5000))
+                                                             .wait_timeout(todo, IDLE_TIME)
                                                              .unwrap();
+                            // do we really need to reassign our MutexGuard??
                             todo = new_lock;
                             !waitres.timed_out()
                         };
 
                         if !received && todo.is_empty() {
-                            return;
+                            return; // redundant thread dies
                         }
                     }
 
@@ -146,7 +162,31 @@ impl TaskPool {
 
 impl Drop for TaskPool {
     fn drop(&mut self) {
+      // make the active_worker_count greater than
+      // MIN_THREADS + total workers do that they'll start shutting down
+      // redundant threads and they'll keep shutting down until they all do!
+      // Better to add a boolean shutdown flag to the Sharing structure!!
         self.sharing.active_tasks.store(999999999, Ordering::Release);
         self.sharing.condvar.notify_all();
     }
 }
+
+/*
+  Proposed new design:
+  A dispatcher sends tasks to workers through a MessageQueue 
+
+  worker loops on
+    receive an Option<Task> -- blocks!
+    None -> terminates
+    Some(task) -> calls it
+
+  dispatcher operations
+    fn add_task(thunk):
+      (1) adjust number of threads
+          creating one if too few (<MIN and >MAX)
+          send None if too many
+      (2) sends a Some(task) into the Message Queue
+    fn shutdown: sends None until all workers terminate
+
+  MIN and MAX should come from toml file!
+*/
